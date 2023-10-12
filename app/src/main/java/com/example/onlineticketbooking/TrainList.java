f72cb753bfa753ad96414e2d9341ea5528df9d13
Sanjula-Dulshan <sdulshan10@gmail.com>
2023-10-12 19:35:04 +0530

    Fixed conflict

--- app/src/main/java/com/example/onlineticketbooking/TrainList.java
+++ app/src/main/java/com/example/onlineticketbooking/TrainList.java
@@ -6,8 +6,8 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import com.example.onlineticketbooking.models.search.SearchResponse;
 
+import com.example.onlineticketbooking.models.search.SearchResponse;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -20,15 +20,15 @@ import java.util.TimeZone;
 public class TrainList extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    private final String loginStateFile = "loginstate";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_train_list);
-
         recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
 
 
         Intent intent = getIntent();
@@ -48,20 +48,17 @@ public class TrainList extends AppCompatActivity {
         List<TrainSchedule> data = new ArrayList<>();
 
         // Check if reservationResponse is not null and contains a list of trains
-
         if (searchResponse != null && searchResponse.getTrainList() != null) {
             for (SearchResponse.Train train : searchResponse.getTrainList()) {
                 String trainId = train.getTrainId();
                 String trainName = train.getTrainName();
 
                 try {
-
                     SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US);
                     inputFormat.setTimeZone(TimeZone.getTimeZone("UTC"));  // Set the time zone to UTC
 
                     Date startDate = inputFormat.parse(train.getStartTime());
                     Date endDate = inputFormat.parse(train.getEndTime());
-
 
                     SimpleDateFormat outputFormat = new SimpleDateFormat("h:mm a", Locale.US);
                     String startTime = outputFormat.format(startDate);
@@ -78,15 +75,6 @@ public class TrainList extends AppCompatActivity {
                     String date = dateTimeParts[0];
 
 
-                    // Create a TrainSchedule object and add it to the data list
-                    TrainSchedule schedule = new TrainSchedule(trainName, startTime, endTime, String.valueOf(noOfSeats));
-                    data.add(schedule);
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-            }
-
-
                     TrainSchedule schedule = new TrainSchedule(trainId, trainName, String.valueOf(availableSeats), startStation, startTime, endStation, endTime, String.valueOf(noOfSeats), String.valueOf(searchResponse.getTotalPrice()), date);
                     data.add(schedule);
                 } catch (ParseException e) {
@@ -95,7 +83,6 @@ public class TrainList extends AppCompatActivity {
             }
 
             TrainAdapter adapter = new TrainAdapter(data);
-
             recyclerView.setAdapter(adapter);
 
         }
