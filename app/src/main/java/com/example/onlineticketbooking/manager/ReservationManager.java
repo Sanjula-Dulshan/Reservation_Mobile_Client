80bfcc9dce19fc4caee2f58e73135006bd3b90bb
Sanjula-Dulshan <sdulshan10@gmail.com>
2023-10-13 09:52:34 +0530

    save reservation

--- app/src/main/java/com/example/onlineticketbooking/manager/ReservationManager.java
+++ app/src/main/java/com/example/onlineticketbooking/manager/ReservationManager.java
@@ -1,6 +1,8 @@
 package com.example.onlineticketbooking.manager;
 
 import androidx.core.util.Consumer;
+
+import com.example.onlineticketbooking.models.reservation.ReservationRequestBody;
 import com.example.onlineticketbooking.models.reservation.ReservationResponse;
 import com.example.onlineticketbooking.models.reservation.ReservationService;
 
@@ -12,7 +14,7 @@ import retrofit2.Response;
 
 public class ReservationManager {
     private static ReservationManager singleton;
-    private ReservationService reservationService;
+    private final ReservationService reservationService;
 
     public static ReservationManager getInstance() {
         if (singleton == null)
@@ -40,7 +42,7 @@ public class ReservationManager {
                     //System.out.println("Number of elements in the list: " + response.body().size());
 
                     //check if response is empty
-                    if(response.body().isEmpty()){
+                    if (response.body().isEmpty()) {
                         System.out.println("No reservations found");
                         onError.accept("No reservations found");
                         return;
@@ -69,5 +71,32 @@ public class ReservationManager {
         });
     }
 
+    public void addReservation(String userId, String trainId, String fromStation, String toStation, int noOfSeats, String date, double totalPrice, Runnable onSuccess, Consumer<String> onError) {
+        if (!NetworkManager.getInstance().isNetworkAvailable()) {
+            onError.accept("No internet connectivity");
+            return;
+        }
 
+        ReservationRequestBody body = new ReservationRequestBody(userId, trainId, fromStation, toStation, noOfSeats, date, totalPrice);
+        reservationService.addReservation(body).enqueue(new Callback<ReservationResponse>() {
+            @Override
+            public void onResponse(Call<ReservationResponse> call, Response<ReservationResponse> response) {
+                if (response.isSuccessful()) {
+                    ReservationResponse reservationResponse = response.body();
+                    if (reservationResponse != null) {
+                        onSuccess.run();
+                    } else {
+                        onError.accept("Unknown error occurred while adding reservation");
+                    }
+                } else {
+                    onError.accept("Failed to add reservation: " + response.message());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ReservationResponse> call, Throwable t) {
+                onError.accept("Unknown error occurred while adding reservation");
+            }
+        });
+    }
 }
